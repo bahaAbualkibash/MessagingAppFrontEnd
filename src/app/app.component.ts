476825408cc62f86b891bbd67469243efd238d09
Baha <baha.ra3d@gmail.com>
2022-02-21 16:13:29 +0200

    FrontEnd Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'MessagingApp';
+export class AppComponent implements OnInit {
+  title = 'The Messaging App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7173/api/users').subscribe(
+      (res) => {
+        this.users = res;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
